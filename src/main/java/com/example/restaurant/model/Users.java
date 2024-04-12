fcfcac397a7fb1bb8738e26203ad337b9888fba8
Mengni Heng <hengmengni2003@gmail.com>
2024-04-12 10:37:16 +0700

    new user class

--- src/main/java/com/example/restaurant/model/Users.java
+++ src/main/java/com/example/restaurant/model/Users.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "users")
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -27,4 +25,5 @@ public class User {
     private String password;
     private String email;
     private String profileImage;
+    private RoleUser role;
 }
